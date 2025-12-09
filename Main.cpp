c3258b15c3ecf256c65a3da1d607f37379efd3b2
B1L_L1E <karol660rata@gmail.com>
2025-12-09 16:32:57 +0100

    dodanie setup

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,32 @@
 
 using namespace std;
 
+int x , y;  //pozycja glowy weza
+int fruitX, fruitY; // pozycja owocu
+int score; // wynik
+const int heightP = 10; //wysokosc planszy
+const int widthP = 10; // szerokosc planszy
+
+bool gameOver = true; // informacja o przegranej w grze czy to prawda czy falsz
+
+void Setup(){
+    gameOver = false;
+    x = 3; //ustawienie pozycji startowej glowy weza
+    y = 3;
+
+    fruitX = 6; // pozycja owocu
+    fruitY = 6;
+
+    score = 0; // resetowanie wyniku
+
+}
+
 int main(){
 
-    //pozycja glowy weza
-    int x , y;
-    int heightP = 10;
-    int widthP = 10; 
+    Setup();
+    while(!gameOver){
+
+    }
+    return 0;
+    
 }
